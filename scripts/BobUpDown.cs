136462d566a4ec9f210202d93969ca9f6a7dda57
PixelDough <alworrell19@gmail.com>
2025-11-05 01:52:14 -0600

    added position offset to bob up down

--- scripts/BobUpDown.cs
+++ scripts/BobUpDown.cs
@@ -9,6 +9,7 @@ public partial class BobUpDown : Node
     [Export] public Curve Curve;
     [Export] public float PositionInfluence = 1.0f;
     private Vector3 _initialPosition;
+    private Vector3 _initialPositionGlobal;
     private Node3D _parent;
     private float _time;
 
@@ -17,13 +18,14 @@ public partial class BobUpDown : Node
         base._Ready();
         _parent = GetParent<Node3D>();
         _initialPosition = _parent.Position;
+        _initialPositionGlobal = _parent.GlobalPosition;
     }
 
     public override void _Process(double delta)
     {
         base._Process(delta);
         _time += (float)delta * Speed;
-        float positionOffset = (_initialPosition.X + _initialPosition.Y) * PositionInfluence;
-        _parent.Position = _initialPosition + Vector3.Up * Height * Curve.Sample(_time % 1f);
+        float positionOffset = (_initialPositionGlobal.X + _initialPositionGlobal.Z + _initialPositionGlobal.Y) * PositionInfluence;
+        _parent.Position = _initialPosition + Vector3.Up * Height * Curve.Sample((_time + positionOffset) % 1f);
     }
 }
