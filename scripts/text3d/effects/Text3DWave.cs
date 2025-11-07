80c2de4f74abb720defb050df58f895cf7ad7945
PixelDough <alworrell19@gmail.com>
2025-11-07 00:35:06 -0600

    added wave effect and effects namespace

--- scripts/text3d/effects/Text3DWave.cs
+++ scripts/text3d/effects/Text3DWave.cs
@@ -3,19 +3,14 @@ using System;
 
 namespace Parallas.Text3D.Effects;
 [GlobalClass, Tool]
-public partial class Text3DSway : Text3DEffect
+public partial class Text3DWave : Text3DEffect
 {
     [Export] public float Speed = 1f;
     [Export] public float IndexOffset = 0f;
-
-    [Export(PropertyHint.Range, "0, 180, 1.0")]
-    public float AngleDegrees = 30;
+    [Export] public float Intensity = 0.2f;
 
     public override Transform3D UpdateRelativeTransform(Rid instance, int index, Transform3D transform, float time, double delta)
     {
-        return transform.RotatedLocal(
-            Vector3.Up,
-            Mathf.Sin(time * Speed + index * IndexOffset) * Mathf.DegToRad(AngleDegrees)
-        );
+        return transform.TranslatedLocal(Vector3.Up * Mathf.Sin(time * Speed + IndexOffset * index) * Intensity);
     }
 }
